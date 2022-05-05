cc8064c5012c88beddb02e5f86294ab5e074c968
andredoc <andre.documet@gmail.com>
2022-05-05 23:47:49 +0200

    chart linear graphic done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -29,16 +29,16 @@ const Featured = () => {
                         </div>
                     </div>
                     <div className="item">
-                        <div className="itemTitle">Target</div>
+                        <div className="itemTitle">Last Week</div>
                         <div className="itemResult positive">
-                            <KeyboardArrowDownIcon fontSize="small" />
+                            <KeyboardArrowUpOutlinedIcon fontSize="small" />
                             <div className="resultAmount">€12.4k</div>
                         </div>
                     </div>
                     <div className="item">
-                        <div className="itemTitle">Target</div>
+                        <div className="itemTitle">Last Month</div>
                         <div className="itemResult positive">
-                            <KeyboardArrowDownIcon fontSize="small" />
+                            <KeyboardArrowUpOutlinedIcon fontSize="small" />
                             <div className="resultAmount">€12.4k</div>
                         </div>
                     </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar= () => {
     return(
         <div className="sidebar">
             <div className="top">
-                <span className="logo">lamadmi</span>
+                <span className="logo">andredoc@</span>
             </div>
             <hr />
             <div className="center">
