9da52d1da3602adced4c0cecc65b027ca2c18c9f
andredoc <andre.documet@gmail.com>
2022-05-10 20:02:26 +0200

    dashboard done

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,21 +1,36 @@
 import './datatable.scss'
-import {userColumns, userRows} from '../../datatablesource'
+import { userColumns, userRows } from '../../datatablesource'
 // From Material UI React Table
 import { DataGrid } from '@mui/x-data-grid';
 import { PanoramaRounded } from '@mui/icons-material';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Datatable = () => {
-    const actionColumn=[ { field: "action", headerName:"Action", width:200, renderCell:()=>{
-        return (
-            <div className="cellAction">
-                <Link to="/users/test" style={{ textDecoration: "none" }} > 
-                    <div className="viewButton">View</div>
-                </Link>
-                <div className="deleteButton">Delete</div>
-            </div>
-        )
-    }}];
+
+    const [data,setData] = useState(userRows)
+
+    const handleDelete = (id)=>{
+        setData(data.filter((item)=> item.id !== id))
+    }
+
+    const actionColumn=[ 
+        { 
+            field: "action", 
+            headerName:"Action", 
+            width:200, 
+            renderCell: (params) => {
+                return (
+                    <div className="cellAction">
+                        <Link to="/users/test" style={{ textDecoration: "none" }} > 
+                            <div className="viewButton">View</div>
+                        </Link>
+                        <div className="deleteButton" onClick={()=>handleDelete(params.row.id)}>Delete</div>
+                    </div>
+                )
+            }
+        }
+    ];
     
     return (
         <div className="datatable">
@@ -27,7 +42,7 @@ const Datatable = () => {
             </div>
             <DataGrid
                 className="datagrid"
-                rows={userRows}
+                rows={data}
                 columns={userColumns.concat(actionColumn)}
                 pageSize={9}
                 rowsPerPageOptions={[9]}
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
 import Chart from '../../components/chart/Chart'
 import List from '../../components/table/Table'
+import Astrid_photo from '../../image/astrid_photo.jpg'
 
 const Single = () => {
     return (
@@ -16,7 +17,8 @@ const Single = () => {
                         <h1 className="title">Information</h1>
                         <div className="item">
                             <img 
-                                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" 
+                                src={Astrid_photo}
+                                // src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" 
                                 alt="" 
                                 className="itemImg" 
                             />
